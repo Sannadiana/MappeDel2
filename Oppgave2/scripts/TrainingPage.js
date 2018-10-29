10eb908753559446fe0e414082752687f9dda4fa
sannadiana <sanna-diana@hotmail.com>
2018-10-29 17:48:53 +0100

    oppdatering

--- Oppgave2/scripts/TrainingPage.js
+++ Oppgave2/scripts/TrainingPage.js
@@ -9,17 +9,44 @@
         const listOfRepitations = document.getElementById("list-of-reps");
 
 
+
         //Array of exercises 
-        const exerciseList = ['Box Jump',
-            'Burpees',
-            'Squat',
-            'Reverse lunge',
-            'Push ups',
-            'Pistol squat',
-            'Squat jump',
-            'Hip thrust',
-            'Glutebridge',
-            'Lying shoulderpress'
+        const exerciseList = [{
+                title: 'Box Jump',
+                img: 'images/boxJump.jpg'
+            },
+            {
+                title: 'Burpees',
+                img: 'images/burpees.jpg'
+            },
+            {
+                title: 'Squat',
+                img: 'images/squat.jpg'
+            },
+            {
+                title: 'Reverse lunge',
+                img: 'images/reverseLunge.jpg'
+            },
+            {
+                title: 'Push ups',
+                img: 'images/PushUps.jpg'
+            }, {
+                title: 'Pistol squat',
+                img: 'images/squat.jpg'
+            }, {
+                title: 'Squat jump',
+                img: 'images/SquatJump.jpg'
+            }, {
+
+                title: 'Hip thrust',
+                img: 'images/HipThrust.png'
+            }, {
+                title: 'Glutebridge',
+                img: 'images/Glutebridg.jpg'
+            }, {
+                title: 'Plank shoulder taps',
+                img: 'images/PlankShoulderTaps.jpg'
+            }
         ];
 
         //events
@@ -35,30 +62,48 @@
             let numberOfListItems = listItems.length;
 
             for (let i = 0; i < numberOfListItems; i++) {
-                getRandomInt(listItems[i]);
-                getRandomExercise(listItems[i]);
+                const reps = getRandomInt();
+                const exercise = getRandomExercise();
+                const img = createImageTag(exercise.img);
+                img.style.height = "100px";
+                img.style.width = "200px";
+
+                listItems[i].textContent = "x" + reps + ": " + exercise.title;
+                listItems[i].appendChild(img)
             }
         }
 
-
-
-
-        function getRandomExercise(listItem) {
-            let exercise = exerciseList[Math.floor(Math.random() * exerciseList.length)];
-            listItem.textContent = listItem.textContent + " " + exercise;
+        function createImageTag(imgUrl) {
+            let image = document.createElement('img');
+            image.classList.add('list-of-reps');
+            image.src = imgUrl;
+            return image
         }
 
 
 
-        function getRandomInt(listItem) {
+        function getRandomExercise() {
+            let exercise = exerciseList[Math.floor(Math.random() * exerciseList.length)];
+            return exercise;
+        }
+
+
+
+        function getRandomInt() {
             const min = 6;
             const max = 20;
 
             let number = Math.floor(Math.random() * (max - min + 1)) + min;
 
-            listItem.textContent = "x" + number + ":";
+            return number;
 
         }
+
+
+
+        const title = exercise.title
+        const img = exercise.img
+
     }
 
 
